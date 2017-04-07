51a7ea26ca696f3660fedaf01091a0b6eb209846
ij8585 <ijanciak@gmail.com>
2017-04-07 16:05:48 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -7,22 +7,148 @@
 #    http://shiny.rstudio.com/
 #
 
+# library(shiny)
+# library(UsingR)
+# data(galton)
+# 
+# shinyServer(  
+#   function(input, output) {    
+#     output$newHist <- renderPlot({      
+#       hist(galton$child, xlab='child height', col='lightblue',main='Histogram')      
+#       mu <- input$mu      
+#       lines(c(mu, mu), c(0, 200),col="red",lwd=5)      
+#       mse <- mean((galton$child - mu)^2)      
+#       text(63, 150, paste("mu = ", mu))      
+#       text(63, 140, paste("MSE = ", round(mse, 2)))      
+#     })      }
+# )
+
+
+
+# 
+# library(shiny)
+# library(mclust)
+# 
+# function(input, output, session) {
+#   
+#   # Create a spot where we can store additional
+#   # reactive values for this session
+#   val <- reactiveValues(x=NULL, y=NULL)    
+#   
+#   # Listen for clicks
+#   observe({
+#     # Initially will be empty
+#     if (is.null(input$clusterClick)){
+#       return()
+#     }
+#     
+#     isolate({
+#       val$x <- c(val$x, input$clusterClick$x)
+#       val$y <- c(val$y, input$clusterClick$y)
+#     })
+#   })
+#   
+#   # Count the number of points
+#   output$numPoints <- renderText({
+#     length(val$x)
+#   })
+#   
+#   # Clear the points on button click
+#   observe({
+#     if (input$clear > 0){
+#       val$x <- NULL
+#       val$y <- NULL
+#     }
+#   })
+#   
+#   # Generate the plot of the clustered points
+#   output$clusterPlot <- renderPlot({
+#     
+#     tryCatch({
+#       # Format the data as a matrix
+#       data <- matrix(c(val$x, val$y), ncol=2)
+#       
+#       # Try to cluster       
+#       if (length(val$x) <= 1){
+#         stop("We can't cluster less than 2 points")
+#       } 
+#       suppressWarnings({
+#         fit <- Mclust(data)
+#       })
+#       
+#       mclust2Dplot(data = data, what = "classification", 
+#                    classification = fit$classification, main = FALSE,
+#                    xlim=c(-2,2), ylim=c(-2,2))
+#     }, error=function(warn){
+#       # Otherwise just plot the points and instructions
+#       plot(val$x, val$y, xlim=c(-2, 2), ylim=c(-2, 2), xlab="", ylab="")
+#       text(0, 0, "Unable to create clusters.\nClick to add more points.")
+#     })
+#   })
+# }
+
+
+
+
 library(shiny)
-library(UsingR)
-data(galton)
+library(datasets)
 
-shinyServer(  
-  function(input, output) {    
-    output$newHist <- renderPlot({      
-      hist(galton$child, xlab='child height', col='lightblue',main='Histogram')      
-      mu <- input$mu      
-      lines(c(mu, mu), c(0, 200),col="red",lwd=5)      
-      mse <- mean((galton$child - mu)^2)      
-      text(63, 150, paste("mu = ", mu))      
-      text(63, 140, paste("MSE = ", round(mse, 2)))      
-    })      }
-)
+# Define server logic required to summarize and view the 
+# selected dataset
+function(input, output) {
+  
+  # Return the requested dataset
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "pressure" = pressure,
+           "women" = women,
+           "cars" = cars)
+  })
+  
+  # Generate a summary of the dataset
+  output$summary <- renderPrint({
+    summary( datasetInput())
+  })
+  
+  # Show the first "n" observations
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
+  
+  
+  
+  output$plot <- renderPlot({
+ 
+  if (input$dataset == "cars") {
+  
+    plot(cars, xlab = "Speed", ylab = "Stopping distance", las = 1, cex = input$size, pch = 19)
+    lines(lowess(cars$speed, cars$dist, f = 2/3, iter = 3), col = input$col)
+    title(main = "Cars data")
+    
+  } 
+    
+  if (input$dataset == "pressure")  {
 
-
-
-
+    plot(pressure, xlab = "Temperature", ylab = "Pressure", las = 1, cex = input$size,  pch = 19)
+    
+    lines(lowess(pressure$temperature, pressure$pressure, f = 2/3, iter = 3), col = input$col)
+    title(main = "Pressure data")
+    
+  } 
+    
+  if  (input$dataset == "women")  {
+    
+    plot(women, xlab = "Height", ylab = "Weight", las = 1, cex = input$size,  pch = 19)
+    
+    lines(lowess(women$height, women$weight, f = 2/3, iter = 3), col = input$col)
+    title(main = "Women data")
+    
+  }    
+    
+    
+  
+    
+  })
+  
+  
+}
--- ui.R
+++ ui.R
@@ -7,18 +7,124 @@
 #    http://shiny.rstudio.com/
 #
 
+
+
+
+
+# library(shiny)
+#  
+# 
+# shinyUI(pageWithSidebar(  
+#   headerPanel("Example plot"),  
+#   sidebarPanel(    
+#     sliderInput('mu', 'Guess at the mu',value = 70, min = 60, max = 80, step = 0.05,)  ), 
+#   mainPanel(    
+#     plotOutput('newHist')  
+#   )
+# ))
+
+
+#install.packages("mclust")
+
+
+# 
+# library(shiny)
+# 
+# # Create a bootstrap fluid layout
+# fluidPage(
+#   
+#   # Add a title
+#   titlePanel("Dynamic Clustering in Shiny"),
+#   
+#   # Add a row for the main content
+#   fluidRow(
+#     
+#     # Create a space for the plot output
+#     plotOutput(
+#       "clusterPlot", "100%", "500px", click="clusterClick"
+#     )
+#   ),
+#   
+#   # Create a row for additional information
+#   fluidRow(
+#     # Take up 2/3 of the width with this element  
+#     mainPanel("Points: ", verbatimTextOutput("numPoints")),
+#     
+#     # And the remaining 1/3 with this one
+#     sidebarPanel(actionButton("clear", "Clear Points"))
+#   )    
+# )
+
+
+
 library(shiny)
+
+# Define UI for dataset viewer application
+fluidPage(
+  
+  # Application title.
+  titlePanel("Developing Data Products: Week 3 Assignment"),
+  
+  
+  h4("ij8585"),
+  h4("April 07, 2017"),
+  
+  helpText("This is data exploration application which presents basic statistics, visualize data and displays some observations of a selected dataset.","Use the control panel on the left to explore the data.,"Use the control panel on the left to explore the data. "), 
  
+  sidebarLayout(
 
-shinyUI(pageWithSidebar(  
-  headerPanel("Example plot"),  
-  sidebarPanel(    
-    sliderInput('mu', 'Guess at the mu',value = 70, min = 60, max = 80, step = 0.05,)  ), 
-  mainPanel(    
-    plotOutput('newHist')  
+    
+
+    sidebarPanel(
+    
+      selectInput("dataset", "Choose a dataset:", 
+                  choices = c("pressure","cars","women")),  
+      
+      helpText("Select a data set for data exploration"), 
+      
+
+      radioButtons("col", "Line color", 
+                  choices = c("blue", "red","green")),
+      
+      
+      helpText("Pickup a color of the line in the data view"), 
+      
+      
+      sliderInput('size', 'Point size', min = 0.5, max = 3, value = 1),
+      
+      helpText("Select a size of the points in the data view"), 
+      
+      numericInput("obs", "Number of observations to view:", 10) 
+      
+      
+
+      
+      #submitButton("Update View") 
+      
+
+    ),
+
+    
+   
+ 
+    mainPanel(
+      
+      h4("Summary"),
+      verbatimTextOutput("summary"),
+      helpText("Note: while the data view will show only the specified",
+               "number of observations, the summary and data view will still be based",
+               "on the full dataset."),  
+      
+      h4("Data View"),
+      plotOutput("plot"),
+      helpText("Note: the size of the points and color of the line can be changed in the control panel on the left"),  
+      
+      
+      h4("Observations"),
+      tableOutput("view"),
+      helpText("Note: the number of the observations can be changed in the control panel on the left")   
+      
+      
+    )
   )
-))
-
-
-
-
+)
